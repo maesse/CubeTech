1b10b092ee3055a2a94762d3734e755f6cc1b688
Mads <mads.lind@gmail.com>
2011-05-05 00:06:48 +0200

    CubeChunk: Changed size from 8 to 32.
    Removed useless visibility culling.
    Fixed VBO using max size by default

--- src/cubetech/collision/CubeChunk.java
+++ src/cubetech/collision/CubeChunk.java
@@ -26,29 +26,24 @@ import org.lwjgl.util.vector.Vector4f;
  * @author mads
  */
 public class CubeChunk {
-    public static final int SIZE = 8;
+    private static final float VBO_RESIZE_MULTIPLIER = 1.3f;
+    public static final int SIZE = 32;
     public static final int CHUNK_SIZE = SIZE*SIZE*SIZE;
     public static final int BLOCK_SIZE = 32;
-    private static final int PLANE_SIZE = 4*32; // (vertex: 3*4, color: 4*1, tex: 2*4) * 4 points
+    public static final float RADIUS = BLOCK_SIZE * SIZE * 1.5f;
+    public static final int PLANE_SIZE = 4*32; // (vertex: 3*4, color: 4*1, tex: 2*4) * 4 points
 
     // block data
     private boolean[] visible = new boolean[CHUNK_SIZE*6]; // plane vis data
     private byte[] blockType = new byte[CHUNK_SIZE];
     public int[] absmin = new int[3];
     public int[] absmax = new int[3];
-    CubeMap map = null;
+    public float[] fcenter = new float[3];
 
     // render
     private VBO vbo = null;
     private boolean dirty = true;
-
-    // vis
-    private int nVis = 0;
-    private boolean[] visBlock = new boolean[CHUNK_SIZE]; // pre-vis data
-    private short[] visBlock2 = new short[CHUNK_SIZE];
-    private static final int MAX_VIS_RELATIONS = 128;
-    boolean[] visRelations = new boolean[MAX_VIS_RELATIONS];
-    int visIndex = 1;
+    public int nSides = 0; // Number of QUAD's that needs to be rendered
 
     // debug draw
     int traceTime = 0;
@@ -59,20 +54,23 @@ public class CubeChunk {
     //int px, py, pz;
     public CubeChunk(CubeMap map, int x, int y, int z) {
         p = new int[] {x,y,z};
-        this.map = map;
         absmin[0] = x * SIZE * BLOCK_SIZE;
         absmin[1] = y * SIZE * BLOCK_SIZE;
         absmin[2] = z * SIZE * BLOCK_SIZE;
         absmax[0] = (x+1) * SIZE * BLOCK_SIZE;
         absmax[1] = (y+1) * SIZE * BLOCK_SIZE;
         absmax[2] = (z+1) * SIZE * BLOCK_SIZE;
+        fcenter[0] = x * SIZE * BLOCK_SIZE + SIZE/2;
+        fcenter[1] = y * SIZE * BLOCK_SIZE + SIZE/2;
+        fcenter[2] = z * SIZE * BLOCK_SIZE + SIZE/2;
     }
 
     public void Render() {
         //RenderSimple();
         markVisible();
-        
-        renderVBO();
+
+        if(nSides > 0)
+            renderVBO();
 
 //        renderDebug();
     }
@@ -98,153 +96,9 @@ public class CubeChunk {
         }
     }
 
-    
-    private void preVIS() {
-        // Clear pre-vis data
-        for (int i= 0; i < visBlock.length; i++) {
-            visBlock[i] = false;
-        }
-
-        rels.clear();
-        
-        visIndex = 1;
-        for (int z= 0; z < SIZE; z++) {
-            for (int y= 0; y < SIZE; y++) {
-                for (int x= 0; x < SIZE; x++) {
-                    int lookup = getIndex(x, y, z);
-                    if(blockType[lookup] != 0) {
-                        visBlock[lookup] = true;
-                        visBlock2[lookup] = -1;
-                        continue;
-                    }
-
-                    boolean boundary = (x == 0 || y == 0 || z == 0);
-                    if(boundary) {
-                        // Set 0, don't check
-                        visBlock2[lookup] = 0;
-                        continue;
-                    }
-
-                    boolean endBoundary = x == SIZE-1 || y == SIZE-1 || z == SIZE-1;
-                    
-                    // X-
-                    short left = -1;
-                    if(x>0 && !visBlock[lookup-1]) {
-                        left = visBlock2[lookup-1];
-                    }
-                    short down = -1;
-                    if(y>0 && !visBlock[lookup-SIZE]) {
-                        down = visBlock2[lookup-SIZE];
-                    }
-                    short back = -1;
-                    if(z>0 && !visBlock[lookup-SIZE*SIZE]) {
-                        back = visBlock2[lookup-SIZE*SIZE];
-                    }
-
-                    int nSolid = back == -1?1:0;
-                    nSolid += down == -1?1:0;
-                    nSolid += left == -1?1:0;
-                    if(nSolid == 3) {
-                        if(endBoundary) {
-                            visBlock2[lookup] = 0;
-                            continue;
-                        }
-                        // Unknown area!
-                        visBlock2[lookup] = (short) visIndex++;
-                        continue;
-                    }
-
-                    if(nSolid == 2) {
-                        if(back != -1) {
-                            visBlock2[lookup] = visBlock2[lookup-SIZE*SIZE];
-                        } else if(down != -1) {
-                            visBlock2[lookup] = visBlock2[lookup-SIZE];
-                        } else if(left != -1) {
-                            visBlock2[lookup] = visBlock2[lookup-1];
-                        }
-                        if(endBoundary) {
-                            // Save 0->vis
-                            rel(0, visBlock2[lookup]);
-                        }
-                        continue;
-                    }
-
-                    if(nSolid == 0 && left == back && left == down) {
-                        // All side are the same type
-                        visBlock2[lookup] = left;
-                        if(endBoundary) {
-                            rel(0,left);
-                        }
-                        continue;
-                    }
-
-                    if(nSolid == 0) {
-                        // 3 sides, different
-                        int max = back > left ? back : left;
-                        max = down > max? down:max;
-                        int min = left < down? left:down;
-                        min = back < min ? back : min;
-
-                        boolean third = back != left && back != down && left != down;
-                        visBlock2[lookup] =(short) min;
-
-                        // Save min->max
-                        rel(min, max);
-                        if(endBoundary) {
-                            rel(0,min);
-                        }
-
-                        if(third) {
-                            int thirdRel = -1;
-                            if(back > min && back < max) thirdRel = back;// save min -> back
-                            else if(left > min && left < max) thirdRel = left;// save min -> left
-                            else if(down > min && down < max) thirdRel = down;// save min -< down
-                            rel(min, thirdRel);
-                        }
-                    } else if(nSolid == 1) {
-                        // 2 sides
-                        int max = back > left ? back : left;
-                        max = down > max? down:max;
-                        int min;
-                        if(back == -1) {
-                            min = left < down? left: down;
-                        } else if(left == -1) {
-                            min = back < down? back:down;
-                        } else {
-                            min = back < left? back:left;
-                        }
-
-                        if(min == max)
-                        {
-                            // Theyre the same
-                            visBlock2[lookup] =(short) min;
-                            if(endBoundary) {
-                                rel(0, min);
-                            }
-                            continue;
-                        }
-
-                        visBlock2[lookup] =(short) min;
-                        
-                        // Save min -> max lookup
-                        rel(min, max);
-                        if(endBoundary) {
-                            rel(0,min);
-                        }
-                    }
-                }
-            }
-        }
-        visRelations = new boolean[visIndex];
-
-
-    }
-
-
-
+  
     public void setCubeType(int x, int y, int z, byte type) {
         setCubeType(getIndex(x, y, z), type);
-
     }
 
     public byte getCubeType(int index)
@@ -255,10 +109,7 @@ public class CubeChunk {
      public void setCubeType(int index, byte type) {
         dirty = true;
         blockType[index] = type;
-        visBlock[index] = type != 0;
     }
-
-    
 
     ChunkSpatialPart lastPart = null;
     ChunkSpatialPart getCubesInVolume(Vector3f start, Vector3f end) {
@@ -313,57 +164,12 @@ public class CubeChunk {
         return part;
     }
 
-    
-    private class RelItem implements Comparable<RelItem> {
-        int min, max;
-        public RelItem(int min, int max) {
-            this.min = min; this.max = max;
-        }
-
-        public int compareTo(RelItem o) {
-            if(min == o.min) return 0;
-            if(min < o.min) return -1;
-            return 1;
-        }
-    }
-    ArrayList<RelItem> rels = new ArrayList<RelItem>();
-    private void rel(int min, int max) {
-        if(max == -1) {
-            int test = 2;
-        }
-        rels.add(new RelItem(min, max));
-    }
-
-    private void postVIS() {
-        Collections.sort(rels);
-        int culledRels = 0;
-        for (RelItem relItem : rels) {
-            int min = relItem.min;
-            if(min != 0 && !visRelations[min]) {
-                culledRels++;
-                continue; // Got something to cull!
-            }
-
-            visRelations[relItem.max] = true;
-        }
-
-        int culledBlocks = 0;
-        for (int i= 0; i < SIZE*SIZE*SIZE; i++) {
-            if(visBlock2[i] > 0 && !visRelations[visBlock2[i]]) {
-                culledBlocks++;
-                visBlock[i] = true;
-            }
-        }
-        Common.Log(""+culledBlocks);
-    }
-
     private void markVisible() {
         if(!dirty)
             return;
 
-        preVIS();
-        postVIS();
-        nVis = 0;
+        nSides = 0;
+
         for (int z= 0; z < SIZE; z++) {
             for (int y= 0; y < SIZE; y++) {
                 for (int x= 0; x < SIZE; x++) {
@@ -373,12 +179,12 @@ public class CubeChunk {
                     }
 
                     // Check all 6 sides
-                    visible[lookup*6] = (x == SIZE-1 || !visBlock[lookup+1]) && (nVis++ >= 0);
-                    visible[lookup*6+1] = (x == 0 || !visBlock[lookup-1]) && (nVis++ >= 0);
-                    visible[lookup*6+2] = (y == SIZE-1 || !visBlock[lookup+SIZE]) && (nVis++ >= 0);
-                    visible[lookup*6+3] = (y == 0 || !visBlock[lookup-SIZE]) && (nVis++ >= 0);
-                    visible[lookup*6+4] = (z == SIZE-1 || !visBlock[lookup+SIZE*SIZE]) && (nVis++ >= 0);
-                    visible[lookup*6+5] = (z == 0 || !visBlock[lookup-SIZE*SIZE]) && (nVis++ >= 0);
+                    visible[lookup*6] = (x == SIZE-1 || blockType[lookup+1] == 0) && (nSides++ >= 0);
+                    visible[lookup*6+1] = (x == 0 || blockType[lookup-1] == 0) && (nSides++ >= 0);
+                    visible[lookup*6+2] = (y == SIZE-1 || blockType[lookup+SIZE] == 0) && (nSides++ >= 0);
+                    visible[lookup*6+3] = (y == 0 || blockType[lookup-SIZE] == 0) && (nSides++ >= 0);
+                    visible[lookup*6+4] = (z == SIZE-1 || blockType[lookup+SIZE*SIZE] == 0) && (nSides++ >= 0);
+                    visible[lookup*6+5] = (z == 0 || blockType[lookup-SIZE*SIZE] == 0) && (nSides++ >= 0);
                 }
             }
         }
@@ -391,12 +197,19 @@ public class CubeChunk {
     }
 
     private void updateVBO() {
-        if(vbo == null)
-            vbo = new VBO(PLANE_SIZE * SIZE*SIZE*SIZE*6, BufferTarget.Vertex);
+        if(nSides == 0) {
+            // TODO: Release VBO
+            return;
+        }
+        if(vbo == null) {
+            // Start off with an exact vbo size.
+            vbo = new VBO(PLANE_SIZE * nSides, BufferTarget.Vertex);
+            // overgrow a bit when resizing
+            vbo.resizeMultiplier = VBO_RESIZE_MULTIPLIER; 
+        }
 
-        ByteBuffer buffer = vbo.map();
+        ByteBuffer buffer = vbo.map(PLANE_SIZE * nSides);
         
-
         try {
             fillBuffer(buffer);
         } catch(BufferOverflowException ex) {
@@ -634,7 +447,7 @@ public class CubeChunk {
         
 
         GL11.glDisable(GL11.GL_BLEND);
-        GL11.glDrawArrays(GL11.GL_QUADS, 0, nVis*4);
+        GL11.glDrawArrays(GL11.GL_QUADS, 0, nSides*4);
         //GL12.glDrawRangeElements(GL11.GL_QUADS, callStart, callEnd-1, callLenght, GL11.GL_UNSIGNED_INT, offset);
         GL11.glEnable(GL11.GL_BLEND);
         postVbo();
