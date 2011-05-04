39252852c6ca8ea0c598ba9b947b894809152255
Mads <mads.lind@gmail.com>
2011-05-04 14:15:29 +0200

    Added utility methods to singleCube

--- src/cubetech/collision/SingleCube.java
+++ src/cubetech/collision/SingleCube.java
@@ -1,5 +1,8 @@
 package cubetech.collision;
 
+import cubetech.gfx.CubeType;
+import cubetech.misc.Ref;
+
 /**
  * Represents a single cube. Since cubes really consist of an chunk+3 bytes for position, this is easier to
  * pass around...
@@ -24,5 +27,64 @@ public class SingleCube {
         y = col.y;
         z = col.z;
         highlightSide = col.hitAxis;
+    }
+
+    /**
+     * Changes the cubetype in the chunk to the given value.
+     * Can also be 0/CubeType.EMPTY..
+     * @param type
+     */
+    public void putBlock(byte type) {
+        if(chunk != null)
+            chunk.setCubeType(x, y, z, type);
+    }
+
+    /**
+     * Removes the block from the chunk (makes it invisible/non-collidable)
+     */
+    public void removeBlock() {
+        chunk.setCubeType(x,y,z, CubeType.EMPTY);
+    }
+
+    /**
+     * Creates a new SingleCube by moving one step in the direction
+     * specified by the side parameter
+     * @param side x = 1, y = 2, z = 3 - multiply by -1 for negative direction.
+     * @return
+     */
+    public SingleCube getHightlightside(int side) {
+        // FIgure out coords for new cube
+        int[] p = new int[] {x, y, z};
+        int sign = (int)Math.signum(side);
+        int index = (int)Math.abs(side);
+        if(index <= 0 || index > 3)
+            return null;
+
+        // move point in the given direction
+        p[index-1] += sign;
+        CubeChunk nChunk = chunk;
+
+        // Check if new cube is in another chunk
+        if(p[index-1] < 0 || p[index-1] >= CubeChunk.SIZE) {
+            // Copy original chunk position
+            int[] chunkPos = new int[3];
+            System.arraycopy(chunk.p, 0, chunkPos, 0, 3);
+
+             // Use index & sign to get the right chunk
+            chunkPos[index-1] += sign;
+            
+            // retrieve the chunk
+            nChunk = Ref.cm.cubemap.getChunk(chunkPos[0], chunkPos[1], chunkPos[2]);
+
+            // correct the cube position for the new chunk
+            p[index-1] = p[index-1] & (CubeChunk.SIZE-1); 
+        }
+
+        // return a singlecube describing the position of the new cube
+        return new SingleCube(nChunk, p[0], p[1], p[2]);
+    }
+
+    public SingleCube getHightlightside() {
+        return getHightlightside(highlightSide);
     }
 }
